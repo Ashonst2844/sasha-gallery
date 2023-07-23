584c257e9f39d5f49def19a16bf3ee77b5ab2807
Ashonst2844 <agusyantosugiyanto@gmail.com>
2023-07-23 21:37:03 +0700

    refactoring the slideshow and add a minim animation

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function loopSlideShow(add) {
+    elem = add + 1
+
+    for (let i = 1; i < elem; i++) {
+        const parent = document.getElementById("parent")
+
+        const crtDiv = document.createElement("div");
+        crtDiv.classList.add("slide-box")
+
+        const crtImg = document.createElement("img");
+        crtImg.setAttribute("src", "img/gallery-content/wallpaper" + i +".jpg");
+        
+        crtDiv.appendChild(crtImg);
+        parent.appendChild(crtDiv)
+    }
+}
+loopSlideShow(5);
+
 var slideTotal = document.getElementsByClassName("slide-box").length - 1
 var slideIndex = 0;
 
@@ -22,11 +40,12 @@ function updateSlides() {
 
     for (let i = 0; i < slide.length; i++) {
         slide[i].classList.remove("visible")
+        slide[i].classList.remove("fade")
         
     }
 
     slide[slideIndex].classList.add("visible")
+    slide[slideIndex].classList.add("fade")
     
 }
-
 updateSlides();
