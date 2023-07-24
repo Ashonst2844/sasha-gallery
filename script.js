c17ffe6aede48d6a5d3437511b7311b5b29c9ff1
Ashonst2844 <agusyantosugiyanto@gmail.com>
2023-07-24 17:34:37 +0700

    add main gallery and add 5 more image to directory

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Looping the slideshow on html element 
 function loopSlideShow(add) {
     elem = add + 1
 
@@ -16,18 +17,31 @@ function loopSlideShow(add) {
 }
 loopSlideShow(5);
 
+function loopGallery() {
+    var totalImage = 10
+    for (let i = 0; i < totalImage; i++) {
+        const parent = document.getElementById("container");
+        const crtImg = document.createElement("img");
+        crtImg.setAttribute("src", "img/gallery-content/wallpaper" + (i + 1) + ".jpg")
+        parent.appendChild(crtImg)
+    }
+}
+loopGallery();
+
 var slideTotal = document.getElementsByClassName("slide-box").length - 1
 var slideIndex = 0;
-
+// event onclick previous button
 function plusSlides() {
     slideIndex += 1;
     updateSlides();
 }
+// event onclick next button
 function minSlides() {
     slideIndex -= 1;
     updateSlides();
 }
 
+// updating slide if button previous/next button clicked
 function updateSlides() {
     var slide = document.getElementsByClassName("slide-box");
 
